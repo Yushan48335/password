0dba356a4275e6f3a9178be71403a2bc44fc0652
Yushan <yushan48335@gmail.com>
2019-12-06 23:47:43 +0800

    Better one

--- password.py
+++ password.py
@@ -1,12 +1,12 @@
 num = 3
-while True:
+while num > 0:
+	num = num - 1
 	password = input("密碼是： ")
 	if password == 'a123456':
 		print('密碼成功')
 		break
 	else:
-		num = num - 1
-		print('密碼錯誤！還有', num, '次機會')
-		if num == 0:
-			break
-
+		if num > 0:
+			print('密碼錯誤！還有', num, '次機會')
+		else:
+			print('您的帳號已鎖住，請聯絡電腦中心')
